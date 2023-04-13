778a642c7afbe39908a6b357e0ef9094a0533374
wrefinity <wrefordmessi@gmail.com>
2023-04-13 01:46:43 +0000

    stats fixations

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
-"""A Python script that provides some stats about Nginx
-logs stored in MongoDB:
-Database: logs
-Collection: nginx
-Display (same as the example):
-first line: x logs where x is the number of documents in this collection
-second line: Methods:
-5 lines with the number of documents with the method =
-["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
-(see example below - warning: it’s a tabulation before each line)
-one line with the number of documents with:
-method=GET
-path=/status
-You can use this dump as data sample: dump.zip
-"""
+"""Provide Log """
 
 
 import pymongo
